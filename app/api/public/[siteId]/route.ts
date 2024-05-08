9d897c37c5f5aa036c9a0b068dd3af6d19cf40f4
xprilion <xprilion@gmail.com>
2024-05-08 10:06:24 +0530

    handle errors

--- app/api/public/[siteId]/route.ts
+++ app/api/public/[siteId]/route.ts
@@ -4,5 +4,8 @@ import {GetSiteByIdPublic} from "@/lib/queries/sites";
 export async function GET(req: NextRequest, { params }: { params: { siteId: string } }) {
 	const siteId = params.siteId;
 	const site = await GetSiteByIdPublic(siteId)
+	if(!site){
+		return NextResponse.json({ error: 'Site not found' }, {status: 404});
+	}
 	return NextResponse.json(site)
 }
